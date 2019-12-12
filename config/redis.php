cc78742dd31f1189a7b066ed1e3b228e88c42a70
fragment123100 <fragment123100@yahoo.com>
2019-12-12 17:01:37 +0800

    商户端单点登入

--- config/redis.php
+++ config/redis.php
@@ -3,9 +3,9 @@
 return [
 
     'host' => \think\facade\Env::get('redis.host'),//IP
-    'port' => \think\facade\Env::get('database.port'),         //端口
-    'database' => \think\facade\Env::get('database.database'),
-    'password' => \think\facade\Env::get('database.password'),  //密码
+    'port' => \think\facade\Env::get('redis.port'),         //端口
+    'database' => \think\facade\Env::get('redis.database'),
+    'password' => \think\facade\Env::get('redis.password'),  //密码
 
 ];
 
