f58802b83e0a592666fee8371d0482e422505258
fragment123100 <fragment123100@yahoo.com>
2019-12-19 13:02:29 +0800

    口令

---
+++ application/agent/behavior/intBehavior.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace app\agent\behavior;
+use think\Controller;
+
+
+/**
+ * 访问前的操作
+ * Class intBehavior
+ * @package app\user\behavior
+ */
+class intBehavior extends Controller
+{
+    public function run()
+    {
+
+        //halt(\app\common\model\SysMenu::getUserMenuApi());
+
+
+        Policy();
+
+      $this->check_param();
+    }
+
+
+    /**
+     * 校验参数
+     */
+    protected function check_param(){
+       $param =   $this->request->param();
+        $this->check_param1($param);
+    }
+    protected function check_param1(&$param){
+        foreach ($param as $k => $v){
+            if(is_array($v)){
+                $this->check_param1($v);
+            }elseif(is_string($v)){
+                $data['param'] = $v;
+
+                if($k == 'email' || $k == 'mailUsername'){
+                    $validate2 = $this->validate($data, 'app\common\validate\Common.email');
+                    if (true !== $validate2) exceptions($validate2);
+                }
+
+                if($k == 'create_at' || $k == 'update_at'){
+                    $v = str_replace("-","",$v);
+                    $v = str_replace(" - ","",$v);
+                }
+
+                //极验
+                if($k == 'geetest_seccode'){
+                    $v = str_replace("|","",$v);
+                }
+/*
+                //url
+                if($k == 'href' ){
+                    $v = str_replace("/","",$v);
+                    $v = str_replace("_","",$v);
+                    $v = str_replace("#","",$v);
+                }
+
+
+                //数组
+                if($k == 'filterRules'  || $k == 'sort'  ){
+                    $v = str_replace("[","",$v);
+                    $v = str_replace("]","",$v);
+                }
+
+                //编辑字段的情况
+                if($k == 'value'  ){
+                    $v = str_replace("@","",$v);
+                    $v = str_replace("http://","",$v);
+                }
+
+
+                $v = str_replace("\n","",$v);//换行的情况
+                $v = str_replace(".","",$v);//ip
+                $v = str_replace("|","",$v);//固定金额
+                */
+                $data['param'] = $v;
+                $data['param_k'] = $k;
+
+                //验证数据
+                $validate1 = $this->validate($data, 'app\common\validate\Common.check_param'); //必须为数组或者字母
+                if (true !== $validate1){
+                    logs(json_encode(array($k=>$v),320),'error');
+
+                    exceptions($validate1);
+                }
+            }else{
+                logs(json_encode(array($k=>$v),320)."|未知参数！",'error');
+                exceptions('未知参数！');
+            }
+        }
+    }
+
+
+
+
+
+}
---
+++ application/agent/controller/api/Menu.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\user\controller\api;
+use app\common\controller\UserController;
+use think\facade\Cache;
+
+class Menu extends UserController
+{
+
+    /**
+     * 根据权限规则生成菜单栏数据
+     * @return \think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function getMenu()
+    {
+        if (!empty(Cache::tag('menu')->get(session('user_info.id') . '_UserMenu'))) {
+            return json(Cache::get(session('user_info.id') . '_UserMenu'));
+        } else {
+            $menu_list = \app\common\model\SysMenu::getUserMenuApi();
+            Cache::tag('menu')->set(session('user_info.id') . '_UserMenu', $menu_list, 86400);
+            return json($menu_list);
+        }
+    }
+
+    /**
+     * 获取顶部菜单栏
+     * @return mixed
+     */
+    public function getNav()
+    {
+        if (!empty(Cache::tag('menu')->get(session('user_info.id') . '_UserMenu'))) {
+            $menu_list = Cache::get(session('user_info.id') . '_UserMenu');
+        } else {
+            $menu_list = \app\common\model\SysMenu::getMenuApi();
+            Cache::tag('menu')->set(session('user_info.id') . '_UserMenu', $menu_list, 86400);
+        }
+        return $menu_list;
+    }
+
+}
+
+
+
+
+
