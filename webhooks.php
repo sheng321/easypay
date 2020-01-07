f2db8e8372759688973b1843106fcc823335e13e
jiashen321@protonmail.com <tanjiashen321>
2020-01-07 15:20:36 +0800

    tanger

--- webhooks.php
+++ webhooks.php
@@ -1,34 +1,53 @@
 <?php
 //本地路径
 $local = '/www/wwwroot/easypay';
-//仓库地址
-$remote = 'https://github.com/sheng321/easypay.git';
 
-//密钥，github是密钥，验证方式跟gitee不一样
-$secret = '123456';
-//获取请求参数
-$request = file_get_contents('php://input');
+//文件排它锁 非阻塞模式
+$fp = fopen($local."/hooklock/git.txt", "w+");
+if(flock($fp,LOCK_EX | LOCK_NB))
+{
+    //仓库地址
+    $remote = 'https://github.com/sheng321/easypay.git';
 
-if (empty($request)) {
-    die('request is empty');
+    //密钥，github是密钥，验证方式跟gitee不一样
+    $secret = '123456';
+    //获取请求参数
+    $request = file_get_contents('php://input');
+
+    if (empty($request)) {
+        die('request is empty');
+    }
+    //获取http 头
+    $headers = getHeaders();
+
+    //github发送过来的签名
+    $hubSignature = $headers['X-Hub-Signature'];
+
+    list($algo, $hash) = explode('=', $hubSignature, 2);
+
+    // 计算签名
+    $payloadHash = hash_hmac($algo, $request, $secret);
+    // 判断签名是否匹配
+    if ($hash != $payloadHash) {
+        die('secret is error');
+    }
+
+    echo shell_exec("cd {$local} && /usr/bin/git pull {$remote} 2>&1");
+
+    flock($fp,LOCK_UN);
+}else{
+    echo "有锁";
 }
-//获取http 头
-$headers = getHeaders();
+fclose($fp);
 
-//github发送过来的签名
-$hubSignature = $headers['X-Hub-Signature'];
-
-list($algo, $hash) = explode('=', $hubSignature, 2);
-
-// 计算签名
-$payloadHash = hash_hmac($algo, $request, $secret);
-// 判断签名是否匹配
-if ($hash != $payloadHash) {
-    die('secret is error');
-}
-
-echo shell_exec("cd {$local} && /usr/bin/git pull {$remote} 2>&1");
 die('done ' . date('Y-m-d H:i:s', time()));
+
+
+
+
+
+
+
 /**
  * @todo 获取头信息
  */
