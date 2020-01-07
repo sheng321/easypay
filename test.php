97e3cd5b1d37357f51057cc88b2e9e19f2b4ddc6
jiashen321@protonmail.com <tanjiashen321>
2020-01-07 15:08:20 +0800

    tanger

--- test.php
+++ test.php
@@ -6,13 +6,9 @@ D:\phpStudy\WWW\www.test1.com\thinkphp\library\think\Cookie.php
 
 setcookie ($name,$value,['expires'=>$expire, 'path'=>$option['path'], 'domain'=>$option['domain'], 'secure'=>$option['secure'], 'httponly'=>$option['httponly'],'samesite'=>$option['samesite']]);
 
-
 $arr2 = array_column($arr, 'name');
 
 $tempArr = array_column($arr, null, 'value');
-
-
-6666666
 
 
 
