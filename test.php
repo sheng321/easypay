662fb70daf0f2e44c8ef8b80d6325eb5b3a901c5
jiashen321@protonmail.com <tanjiashen321>
2020-01-07 18:34:19 +0800

    tanger

--- test.php
+++ test.php
@@ -1,16 +1,7 @@
-php.ini
-samesite只有在相同网站中才会填充cookie。避免CSRF
-session.cookie_samesite=Strict
 
-D:\phpStudy\WWW\www.test1.com\thinkphp\library\think\Cookie.php
-
-setcookie ($name,$value,['expires'=>$expire, 'path'=>$option['path'], 'domain'=>$option['domain'], 'secure'=>$option['secure'], 'httponly'=>$option['httponly'],'samesite'=>$option['samesite']]);
-
-$arr2 = array_column($arr, 'name');
 
 $tempArr = array_column($arr, null, 'value');
 
-77777777777777
 
 
 
