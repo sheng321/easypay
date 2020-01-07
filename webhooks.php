51a8903fd2a04b0d483ae199712f4bbc14a17186
jiashen321@protonmail.com <tanjiashen321>
2020-01-07 13:57:24 +0800

    tanger

--- webhooks.php
+++ webhooks.php
@@ -1,34 +1,4 @@
 <?php
-
-$request = '{"ref":"refs/heads/master","before":"02b3cec9f72534d172fc01679eb710f1d7e5b77a","after":"373d3804106ef02199fe3f1a5583686cc140e09d","repository":{"id":227524972,"node_id":"MDEwOlJlcG9zaXRvcnkyMjc1MjQ5NzI=","name":"easypay","full_name":"sheng321/easypay","private":false,"owner":{"name":"sheng321","email":"57615250+sheng321@users.noreply.github.com","login":"sheng321","id":57615250,"node_id":"MDQ6VXNlcjU3NjE1MjUw","avatar_url":"https://avatars1.githubusercontent.com/u/57615250?v=4","gravatar_id":"","url":"https://api.github.com/users/sheng321","html_url":"https://github.com/sheng321","followers_url":"https://api.github.com/users/sheng321/followers","following_url":"https://api.github.com/users/sheng321/following{/other_user}","gists_url":"https://api.github.com/users/sheng321/gists{/gist_id}","starred_url":"https://api.github.com/users/sheng321/starred{/owner}{/repo}","subscriptions_url":"https://api.github.com/users/sheng321/subscriptions","organizations_url":"https://api.github.com/users/sheng321/orgs","repos_url":"https://api.github.com/users/sheng321/repos","events_url":"https://api.github.com/users/sheng321/events{/privacy}","received_events_url":"https://api.github.com/users/sheng321/received_events","type":"User","site_admin":false},"html_url":"https://github.com/sheng321/easypay","description":null,"fork":false,"url":"https://github.com/sheng321/easypay","forks_url":"https://api.github.com/repos/sheng321/easypay/forks","keys_url":"https://api.github.com/repos/sheng321/easypay/keys{/key_id}","collaborators_url":"https://api.github.com/repos/sheng321/easypay/collaborators{/collaborator}","teams_url":"https://api.github.com/repos/sheng321/easypay/teams","hooks_url":"https://api.github.com/repos/sheng321/easypay/hooks","issue_events_url":"https://api.github.com/repos/sheng321/easypay/issues/events{/number}","events_url":"https://api.github.com/repos/sheng321/easypay/events","assignees_url":"https://api.github.com/repos/sheng321/easypay/assignees{/user}","branches_url":"https://api.github.com/repos/sheng321/easypay/branches{/branch}","tags_url":"https://api.github.com/repos/sheng321/easypay/tags","blobs_url":"https://api.github.com/repos/sheng321/easypay/git/blobs{/sha}","git_tags_url":"https://api.github.com/repos/sheng321/easypay/git/tags{/sha}","git_refs_url":"https://api.github.com/repos/sheng321/easypay/git/refs{/sha}","trees_url":"https://api.github.com/repos/sheng321/easypay/git/trees{/sha}","statuses_url":"https://api.github.com/repos/sheng321/easypay/statuses/{sha}","languages_url":"https://api.github.com/repos/sheng321/easypay/languages","stargazers_url":"https://api.github.com/repos/sheng321/easypay/stargazers","contributors_url":"https://api.github.com/repos/sheng321/easypay/contributors","subscribers_url":"https://api.github.com/repos/sheng321/easypay/subscribers","subscription_url":"https://api.github.com/repos/sheng321/easypay/subscription","commits_url":"https://api.github.com/repos/sheng321/easypay/commits{/sha}","git_commits_url":"https://api.github.com/repos/sheng321/easypay/git/commits{/sha}","comments_url":"https://api.github.com/repos/sheng321/easypay/comments{/number}","issue_comment_url":"https://api.github.com/repos/sheng321/easypay/issues/comments{/number}","contents_url":"https://api.github.com/repos/sheng321/easypay/contents/{+path}","compare_url":"https://api.github.com/repos/sheng321/easypay/compare/{base}...{head}","merges_url":"https://api.github.com/repos/sheng321/easypay/merges","archive_url":"https://api.github.com/repos/sheng321/easypay/{archive_format}{/ref}","downloads_url":"https://api.github.com/repos/sheng321/easypay/downloads","issues_url":"https://api.github.com/repos/sheng321/easypay/issues{/number}","pulls_url":"https://api.github.com/repos/sheng321/easypay/pulls{/number}","milestones_url":"https://api.github.com/repos/sheng321/easypay/milestones{/number}","notifications_url":"https://api.github.com/repos/sheng321/easypay/notifications{?since,all,participating}","labels_url":"https://api.github.com/repos/sheng321/easypay/labels{/name}","releases_url":"https://api.github.com/repos/sheng321/easypay/releases{/id}","deployments_url":"https://api.github.com/repos/sheng321/easypay/deployments","created_at":1576127444,"updated_at":"2020-01-07T05:26:31Z","pushed_at":1578375248,"git_url":"git://github.com/sheng321/easypay.git","ssh_url":"git@github.com:sheng321/easypay.git","clone_url":"https://github.com/sheng321/easypay.git","svn_url":"https://github.com/sheng321/easypay","homepage":null,"size":12342,"stargazers_count":0,"watchers_count":0,"language":"PHP","has_issues":true,"has_projects":true,"has_downloads":true,"has_wiki":true,"has_pages":false,"forks_count":0,"mirror_url":null,"archived":false,"disabled":false,"open_issues_count":0,"license":null,"forks":0,"open_issues":0,"watchers":0,"default_branch":"master","stargazers":0,"master_branch":"master"},"pusher":{"name":"sheng321","email":"57615250+sheng321@users.noreply.github.com"},"sender":{"login":"sheng321","id":57615250,"node_id":"MDQ6VXNlcjU3NjE1MjUw","avatar_url":"https://avatars1.githubusercontent.com/u/57615250?v=4","gravatar_id":"","url":"https://api.github.com/users/sheng321","html_url":"https://github.com/sheng321","followers_url":"https://api.github.com/users/sheng321/followers","following_url":"https://api.github.com/users/sheng321/following{/other_user}","gists_url":"https://api.github.com/users/sheng321/gists{/gist_id}","starred_url":"https://api.github.com/users/sheng321/starred{/owner}{/repo}","subscriptions_url":"https://api.github.com/users/sheng321/subscriptions","organizations_url":"https://api.github.com/users/sheng321/orgs","repos_url":"https://api.github.com/users/sheng321/repos","events_url":"https://api.github.com/users/sheng321/events{/privacy}","received_events_url":"https://api.github.com/users/sheng321/received_events","type":"User","site_admin":false},"created":false,"deleted":false,"forced":false,"base_ref":null,"compare":"https://github.com/sheng321/easypay/compare/02b3cec9f725...373d3804106e","commits":[{"id":"373d3804106ef02199fe3f1a5583686cc140e09d","tree_id":"e6801680b5a68f2dcf84628bfbc90d5d0bfa96c1","distinct":true,"message":"tanger","timestamp":"2020-01-07T13:33:52+08:00","url":"https://github.com/sheng321/easypay/commit/373d3804106ef02199fe3f1a5583686cc140e09d","author":{"name":"jiashen321@protonmail.com","email":"tanjiashen321"},"committer":{"name":"jiashen321@protonmail.com","email":"tanjiashen321"},"added":[],"removed":[],"modified":["webhooks.php"]}],"head_commit":{"id":"373d3804106ef02199fe3f1a5583686cc140e09d","tree_id":"e6801680b5a68f2dcf84628bfbc90d5d0bfa96c1","distinct":true,"message":"tanger","timestamp":"2020-01-07T13:33:52+08:00","url":"https://github.com/sheng321/easypay/commit/373d3804106ef02199fe3f1a5583686cc140e09d","author":{"name":"jiashen321@protonmail.com","email":"tanjiashen321"},"committer":{"name":"jiashen321@protonmail.com","email":"tanjiashen321"},"added":[],"removed":[],"modified":["webhooks.php"]}}';
-
-
-//github发送过来的签名
-$hubSignature = 'sha1=1b131953a0fa044ea270b44d94a50c6590f3450a';
-
-list($algo, $hash) = explode('=', $hubSignature, 2);
-
-
-echo 11;
-
-
-// 计算签名
-$payloadHash = hash_hmac($algo, $request, '123456');
-// 判断签名是否匹配
-if ($hash != $payloadHash) {
-    die('secret is error');
-}
-die('111111');
-
-
-
-
-
-
-
-
-
-
 //本地路径
 $local = '/www/wwwroot/easypay';
 //仓库地址
@@ -39,23 +9,16 @@ $secret = '123456';
 //获取请求参数
 $request = file_get_contents('php://input');
 
-
-file_put_contents('/www/wwwroot/easypay/secret1.text',$request);
-
 if (empty($request)) {
     die('request is empty');
 }
 //获取http 头
 $headers = getHeaders();
 
-file_put_contents('/www/wwwroot/easypay/secret.text',json_encode($headers));
-
 //github发送过来的签名
 $hubSignature = $headers['X-Hub-Signature'];
 
-file_put_contents('/www/wwwroot/secret.text',$hubSignature);
 list($algo, $hash) = explode('=', $hubSignature, 2);
-
 
 // 计算签名
 $payloadHash = hash_hmac($algo, $request, $secret);
