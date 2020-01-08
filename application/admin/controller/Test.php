81bddc20a6dcd80429570a60a718803dd2be49b1
jiashen321@protonmail.com <tanjiashen321>
2020-01-08 15:04:01 +0800

    tanger

--- application/admin/controller/Test.php
+++ application/admin/controller/Test.php
@@ -3,16 +3,25 @@ namespace app\admin\controller;
 
 use think\Controller;
 use redis\StringModel;
+use think\Queue;
 
 class Test  extends Controller
 {
     public function index()
     {
+        $job = 'app\\common\\job\\Notify';//调用的任务名
+        $data = 666666;//传入的数据
+        $queue = 'notify';//队列名，可以理解为组名
+        //push()方法是立即执行
+        $res =  Queue::push($job, $data, $queue);
+
+
+
         $model = (new StringModel())->instance();
 
-
         dump(  $model->select(3));
-        $res =   $model->mget('*');
+        dump(1);
+        $res =   $model->keys('queues:notify' . "*");
         halt($res);
 
 
