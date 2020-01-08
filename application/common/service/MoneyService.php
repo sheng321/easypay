2549d928ac177d4bb5dffb4986671a4b21be0328
jiashen321@protonmail.com <tanjiashen321>
2020-01-08 09:52:31 +0800

    tanger

--- application/common/service/MoneyService.php
+++ application/common/service/MoneyService.php
@@ -180,7 +180,7 @@ class MoneyService {
 
         $channel_money  = $Umoney::quickGet(['uid'=>0,'channel_id'=>$Channel['id']]); //通道金额
         if(empty($channel_money)){
-            $Umoney::save(['uid'=>0,'channel_id'=>$Channel['id']]);
+            $Umoney->save(['uid'=>0,'channel_id'=>$Channel['id']]);
         }
 
 
