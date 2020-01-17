e4c7e4128188d3d6508339d7e38d77b360189808
jiashen321@protonmail.com <tanjiashen321>
2020-01-17 10:15:07 +0800

    tanger

--- application/common/job/Notify.php
+++ application/common/job/Notify.php
@@ -14,6 +14,7 @@ class Notify {
         //错误添加到订单回调日志
         logs($data,$type = 'order/notify/'.$data['order']['code']);
 
+
         $Order =  Order::quickGet($data['order']['id']);
         if(empty($Order)){
             $job->delete();
