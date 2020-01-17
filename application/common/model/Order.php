955eb512fa15b168acc51ae62ff2116916b81509
jiashen321@protonmail.com <tanjiashen321>
2020-01-17 10:32:21 +0800

    tanger

--- application/common/model/Order.php
+++ application/common/model/Order.php
@@ -234,7 +234,7 @@ class Order extends ModelService {
 
 
     //订单 回调数据
-    public static function notify($sn,$code){
+    public static function notify($sn,$code = ''){
         $Order = self::quickGet(['systen_no'=>$sn]);
         $Uprofile = Uprofile::quickGet(['uid'=>$Order['mch_id']]);
 
