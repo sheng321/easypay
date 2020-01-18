b295a042d825bb80e427f17ff226aeefa1d066ac
jiashen321@protonmail.com <tanjiashen321>
2020-01-18 13:42:10 +0800

    tanger

--- application/user/controller/Withdrawal.php
+++ application/user/controller/Withdrawal.php
@@ -18,77 +18,9 @@ class Withdrawal extends UserController {
         parent::__construct();
         $this->model = model('app\common\model\Withdrawal');
     }
+
     /**
-     * Undocumented 银行卡
-     *
-     * @return void
-     */
-    public function bank(){
-        if ($this->request->get('type') == 'ajax') {
-            $page = $this->request->get('page', 1);
-            $limit = $this->request->get('limit', 10);
-            $search = (array)$this->request->get('search', []);
-            $count = Db::name("bank_card")->where(array_filter($search))->count();
-            $list = Db::name("bank_card")->where(array_filter($search))->limit(($page-1),$limit)->select();
-            empty($data) ? $msg = '暂无数据！' : $msg = '查询成功！';
-            $data = [
-                'code'  => 0,
-                'msg'   => $msg,
-                'count' => $count,
-                'info'  => ['limit'=>$limit,'page_current'=>$page,'page_sum'=>ceil($count / $limit)],
-                'data'  => $list,
-            ];
-            return json($data);
-        }
-        return view("withdrawal/bank");
-    }
-    /**
-     * Undocumented 添加/编辑银行卡
-     *
-     * @return void
-     */
-    public function saveBark(){
-        $info = array();
-        if($this->request->isPost()){
-            $data = $this->request->param();
-            unset($data['bank_id']);unset($data['__token__']);
-            if(!empty($this->request->param('bank_id'))){//编辑
-                $result = Db::name("bank_card")->where("id",$this->request->param('bank_id'))->update($data);
-            }else{//新增
-                $data['mch_id'] = $this->user['uid'];
-                $result = Db::name("bank_card")->insert($data);
-            }
-            if($result){
-                return __success('操作成功');
-            }
-            return __error('操作失败');
-        }else{//查看
-            $info = Db::name("bank_card")->where("id",$this->request->param('id'))->find();
-        }
-        $this->assign("info",$info);
-        return view("withdrawal/save_bark");
-    }
-    /**
-     * Undocumented 删除银行卡
-     *
-     * @return void
-     */
-    public function delBank(){
-        $id = $this->request->param('id');
-       if(!is_array($id)){
-            $info = Db::name("bank_card")->where("id",$id)->find();
-            if(empty($info)) return __error('数据不存在');
-       }
-        $result = Db::name("bank_card")->where("id",'in',$id)->delete();
-        if($result){
-            return __success('删除成功');
-        }
-        return __error('删除失败');
-    }
-    /**
-     * Undocumented 提现记录
-     *
-     * @return void
+     *  提现记录
      */
     public function index(){
         if ($this->request->get('type') == 'ajax') {
@@ -100,9 +32,7 @@ class Withdrawal extends UserController {
         return view('index');
     }
     /**
-     * Undocumented 申请提现
-     *
-     * @return void
+     *  申请提现
      */
     public function addWithdrawal(){
         if($this->request->isPost()){//提现申请
