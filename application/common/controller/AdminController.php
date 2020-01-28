5e700455e68bd3623b0f52e353e2c68bb174d0fb
jiashen321@protonmail.com <tanjiashen321>
2020-01-28 14:23:13 +0800

    tanger

--- application/common/controller/AdminController.php
+++ application/common/controller/AdminController.php
@@ -96,7 +96,7 @@ class AdminController extends BaseController
             __log( session('admin_info.username').'登入IP白名单不包含此IP:'.get_client_ip());
             session('admin_info', null);
             $data = ['type' => 'error', 'code' => 0, 'msg' =>'登入IP白名单不包含此IP:'.get_client_ip(), 'url' => url('@admin/login/index')];
-            exceptions($data);
+          //  exceptions($data);
         }
     }
 
