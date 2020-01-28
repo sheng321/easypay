a9f9c6022c5afc5aa98010516fdb8d5e70b50386
jiashen321@protonmail.com <tanjiashen321>
2020-01-28 14:35:23 +0800

    tanger

--- application/pay/controller/Api.php
+++ application/pay/controller/Api.php
@@ -63,11 +63,11 @@ class Api extends PayController
 
         //所有在线的支付通道分组及其支付通道
         $ChannelProduct = [];
-        halt($channel_group_idArr[$PayProduct['id']] );
         foreach ($channel_group_idArr[$PayProduct['id']] as $k => $v){
             $temp1['group_id'] = $v;
             $temp1['p_id'] = $PayProduct['id'];
             $temp = ChannelProduct::where($temp1)->select()->toArray();
+            halt($temp);
             if(!empty($temp)){
                 $ChannelProduct =  array_merge($ChannelProduct,$temp);
             }
