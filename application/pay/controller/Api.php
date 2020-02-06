8d8945e557c7013be9820b35ac2bda6726bcfeee
jiashen321@protonmail.com <tanjiashen321>
2020-02-06 12:26:24 +0800

    tanger

--- application/pay/controller/Api.php
+++ application/pay/controller/Api.php
@@ -97,7 +97,6 @@ class Api extends PayController
                 continue;
             }
 
-
             //判断是否国内IP
             if($Channel['forbid'] == 0 && !is_china()){
                 unset($ChannelProduct[$k]);
