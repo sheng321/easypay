4e6637b72a9c9accffe48c4bcfc71354d3bc1558
jiashen321@protonmail.com <tanjiashen321>
2020-02-06 11:52:46 +0800

    tanger

--- application/common.php
+++ application/common.php
@@ -417,7 +417,8 @@ if (!function_exists('get_location')) {
         $location =  \think\facade\Cache::remember('get_location_'.$ip, function () use($ip) {
             $Ip = new \tool\IpLocation(); // 实例化类 参数表示IP地址库文件
             $location =  $Ip->getlocation($ip);// 获取某个IP地址所在的位置
-            \think\facade\Cache::tag('ip')->set('get_location_'.$Ip,$location,60);
+            halt($location);
+            \think\facade\Cache::tag('ip')->set('get_location_'.$Ip,$location,1);
             return \think\facade\Cache::get('get_location_'.$Ip);
         });
         halt($location);
