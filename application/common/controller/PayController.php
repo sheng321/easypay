02622b3704964b327b708f45fea6edb0a0a74b34
jiashen321@protonmail.com <tanjiashen321>
2020-02-06 15:26:51 +0800

    tanger

--- application/common/controller/PayController.php
+++ application/common/controller/PayController.php
@@ -83,7 +83,7 @@ class PayController extends BaseController
                 $Payment = Payment::factory($code);
                 $res = $Payment->repertory();
                 dump($res);
-                \think\facade\Cache::clear('charge');
+                \think\facade\Cache::rm('charge_num_'.$id);
 
                 $num =  \think\facade\Cache::tag('charge')->remember('charge_num_'.$id, function () use($code,$id,$num) {
                    try{
