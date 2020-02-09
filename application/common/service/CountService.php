bec47b65f086503869fa56b2421c1fa69eefb88f
jiashen321@protonmail.com <tanjiashen321>
2020-02-09 14:41:58 +0800

    tanger

--- application/common/service/CountService.php
+++ application/common/service/CountService.php
@@ -26,7 +26,8 @@ class CountService {
         dump($three);
         dump($ten);
 
-        $sql = "select count(1) from cm_order where  create_at BETWEEN ? AND ?";
+        //总订单数 total_orders //总订单金额 total_fee_all //已支付金额 total_fee_paid //已支付订单数 total_paid
+        $sql = "select count(1) as total_orders,COALESCE(sum(amount),0) as total_fee_all,COALESCE(sum(if(pay_status=2,amount,0)),0) as total_fee_paid,COALESCE(sum(if(pay_status=2,1,0)),0) as total_paid from cm_order where  create_at BETWEEN ? AND ? GROUP BY channel_id";//每个通道的成功率
         $select =  Db::query($sql, [$ten,$three]);
 
         halt($select);
