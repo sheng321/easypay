41e1a324f4790112f56ee81c1a22aabf924194ee
jiashen321@protonmail.com <tanjiashen321>
2020-02-09 14:26:51 +0800

    tanger

--- application/admin/controller/Test.php
+++ application/admin/controller/Test.php
@@ -13,28 +13,9 @@ class Test  extends Controller
 {
     public function index()
     {
+        $data =  \app\common\service\CountService::success_rate();
+        halt($data);
 
-        $value='http://admin_xyf987abc.xinyufu.com/Pay_Shen_callbackurl.html';
-         $str="/^http(s?):\/\/(?:[A-za-z0-9-]+\.)+[A-za-z]{2,4}(?:[\/\?#][\/=\?%\-&~`@[\]\':+!\.#\w]*)?$/";
-        if( preg_match($str,$value)) return true;
-
-        $urlarr = parse_url($value);
-        if(filter_var($urlarr['host'], FILTER_VALIDATE_IP)) return true;
-        return false;
-
-
-       dump($urlarr['scheme'].'://'.$urlarr['host']);
-        if(filter_var($urlarr['host'], FILTER_VALIDATE_IP) || preg_match($str,$url_name)){
-            dump(1);
-        }
-        dump(2);
-
-        halt($urlarr);
-
-
-
-
-        return $this->fetch('', []);
     }
 
 }
---
+++ application/common/service/CountService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\common\service;
+use app\common\model\Channel;
+use app\common\model\Order;
+use app\common\model\Umoney;
+use think\Db;
+
+
+/**
+ * 统计
+ * @package service
+ */
+class CountService {
+
+    /**
+     * 通道成功率 3-10分钟的成功率 3分钟统计一次
+     * @return bool
+     */
+    public static function success_rate(){
+        $data = [];
+        $three = timeToDate(0,3);//三分钟前的时间
+        //$ten =  timeToDate(0,10);//十分钟
+        $ten = timeToDate(0,0,0,-14);
+
+        $sql = "select * from cm_order where  create_at  BETWEEN ? AND  ?";
+        $select =  Db::query($sql, [$three, $ten]);
+
+        halt($select);
+
+        return $data;
+    }
+
+
+
+
+
+}
