0a3f956882d6296ab02828615dad9afcce304d5b
jiashen321@protonmail.com <tanjiashen321>
2020-02-14 14:58:22 +0800

    tanger

--- application/admin/controller/Member.php
+++ application/admin/controller/Member.php
@@ -646,6 +646,8 @@ class Member extends AdminController {
      * @return mixed|string|\think\response\Json
      */
     public function edit() {
+
+        dump($this->request->param('id/d',0));
         //查找所需修改用户
         $Member = $this->model->where('id', $this->request->param('id/d',0))->find();
         if (empty($Member)) return exceptions('暂无数据，请重新刷新页面！');
