eb71ecd21751d3d23ce6f71b5db602a3ac264f57
jiashen321@protonmail.com <tanjiashen321>
2020-02-28 12:26:54 +0800

    tanger

--- application/admin/controller/Accounts.php
+++ application/admin/controller/Accounts.php
@@ -140,8 +140,6 @@ class Accounts  extends AdminController
         $Channel =   \app\common\model\Channel::idRate();//通道
         $PayProduct =  \app\common\model\PayProduct::idArr();//支付产品
 
-        halt($PayProduct);
-
 
         $Channel_data = [];
         foreach ($Channel as $k =>$v){
