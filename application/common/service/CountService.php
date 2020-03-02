846e43530cdbf5ffa21647693fe1a678a2267dad
jiashen321@protonmail.com <tanjiashen321>
2020-03-02 15:23:01 +0800

    tanger

--- application/common/service/CountService.php
+++ application/common/service/CountService.php
@@ -848,7 +848,6 @@ class CountService {
                 }
             }
 
-            dump($update);
             //插入每日对账表
             if (!empty($insert)) $Accounts->isUpdate(false)->saveAll($insert);
             if (!empty($update)) $Accounts->isUpdate(true)->saveAll($update);
