71d54e3dbdd545f56bc0fe9c00838b0927a0ec4c
jiashen321@protonmail.com <tanjiashen321>
2020-03-03 12:46:04 +0800

    tanger

--- application/common/controller/AgentController.php
+++ application/common/controller/AgentController.php
@@ -131,8 +131,7 @@ class AgentController extends BaseController
     protected function __checkAuth()
     {
         if (\app\common\service\AuthService::checkAgentNode() == false){
-
-            $data = ['type' => 'error', 'code' => 0, 'msg' => '抱歉，您暂无该权限，请联系管理员！', 'url' => url('@agent/index')];
+            $data = ['type' => 'error', 'code' => 0, 'msg' => '抱歉，您暂无该权限，请联系管理员！', 'url' => ''];
             exceptions($data);
         }
     }
