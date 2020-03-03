239d11a0bae8f97af5eb18c97b334098c012db1b
jiashen321@protonmail.com <tanjiashen321>
2020-03-03 11:13:51 +0800

    tanger

--- config/app.php
+++ config/app.php
@@ -80,8 +80,7 @@ return [
     // 默认的空模块名
     'empty_module'           => '',
     // 默认的空控制器名
-    //'empty_controller'       => 'Error',
-    'empty_controller'       => 'Index',
+    'empty_controller'       => 'Error',
     // 操作方法前缀
     'use_action_prefix'      => false,
     // 操作方法后缀
