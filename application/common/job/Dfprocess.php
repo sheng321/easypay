473c620b6deda9bdbded1cb129d74599a80e5451
jiashen321@protonmail.com <tanjiashen321>
2020-03-04 10:21:30 +0800

    tanger

---
+++ application/common/job/Dfprocess.php
@@ -0,0 +1,113 @@
+<?php
+namespace app\common\job;
+use think\queue\Job;
+use app\common\model\Df;
+use app\withdrawal\service\Payment;
+
+class Dfprocess {
+    /**
+     * fire方法是消息队列默认调用的方法
+     * @param Job            $job      当前的任务对象
+     * @param array|mixed    $data     发布任务时自定义的数据
+     */
+    public function fire(Job $job,$data)
+    {
+        ini_set('max_execution_time','120');
+        $order =  Df::quickGet($data['order']['id']);
+        // 有些消息在到达消费者时,可能已经不再需要执行了
+        $isJobStillNeedToBeDone = $this->checkDatabaseToSeeIfJobNeedToBeDone($data,$order);
+        if(!$isJobStillNeedToBeDone){
+            $job->delete();
+            return;
+        }
+
+        $isJobDone = $this->doHelloJob($data,$order);
+        if ($isJobDone) {
+            // 如果任务执行成功，删除任务
+            $job->delete();
+            return;
+        }else{
+            if ($job->attempts() > 1) {//只执行一次
+                $job->delete();
+                return;
+            }
+        }
+    }
+
+    /**
+     * 有些消息在到达消费者时,可能已经不再需要执行了
+     * @param array|mixed    $data     发布任务时自定义的数据
+     * @return boolean                 任务执行的结果
+     */
+    private function checkDatabaseToSeeIfJobNeedToBeDone($data,$order){
+
+        if(empty($order) || $order['status'] != 1 || $order['lock_id'] != 0){
+           return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * 根据消息中的数据进行实际的业务处理...
+     */
+    private function doHelloJob($data,$order)
+    {
+        $this->model = model('app\common\model\Df');
+
+        //使用事物保存数据
+        $this->model->startTrans();
+        //选择通道并且处理中
+        $save1 =  $this->model->save($data['order'],['id'=>$data['order']['id']]);
+        $save = model('app\common\model\Umoney')->isUpdate(true)->saveAll($data['Umoney']);
+        $add = model('app\common\model\UmoneyLog')->isUpdate(false)->saveAll($data['UmoneyLog']);
+
+        if ( !$save1 || !$save || !$add ) {
+            $this->model->rollback();
+            $msg = '数据更新失败，请稍后再试!';
+        }
+
+        $Payment = Payment::factory($data['channel']['code']);
+
+        //这里提交代付申请
+        $order['channel_amount'] = $data['order']['channel_amount'];
+        $order['bank'] = json_decode($order['bank'],true);
+        $result = $Payment->pay($order);
+        if(empty($result)|| !is_array($result)){
+            $this->model->rollback();
+            $msg = '代付通道异常，请稍后再试!';
+        }
+
+        //成功
+        if($result['code'] == 1){
+            //更新数据
+            if(!empty($result['data']) && is_array($result['data'])){
+                $arr = [];
+                foreach ($result['data'] as $k1 => $v1){
+                    if($k1 == 'actual_amount') $arr[$k1] = $v1;//实际到账
+                    if($k1 == 'transaction_no') $arr[$k1] = $v1;//上游单号
+                    if($k1 == 'remark') $arr[$k1] = $v1;//备注
+                }
+                if(!empty($arr)){
+                    $arr['id'] = $data['order']['id'];
+                    $this->model->save($arr,['id'=>$data['order']['id']]);
+                }
+            }
+
+            $this->model->commit();
+            //添加异步查询订单状态
+            \think\Queue::later(60,'app\\common\\job\\Df', $order['id'], 'df');//一分钟
+
+        }else{
+            $this->model->rollback();
+            $msg = '申请代付失败，请检查上游订单状，上游返回：'.$result['msg']."，失败\n";
+        }
+
+        if(!empty($msg)){
+            $this->model->save(['id'=>$data['order']['id'],'remark'=>$msg],['id'=>$data['order']['id']]);
+            return false;
+        }
+        
+        return true;
+    }
+}
