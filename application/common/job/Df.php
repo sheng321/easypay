11b1ebda43ca4f1635d19299ff000d541f19abf7
jiashen321@protonmail.com <tanjiashen321>
2020-03-05 15:35:29 +0800

    tanger

--- application/common/job/Df.php
+++ application/common/job/Df.php
@@ -100,6 +100,7 @@ class Df {
 
             $change['change'] = $Order['channel_amount'];//通道变动金额
             $res2 = Umoney::dispose($channel_money, $change); //通道处理
+            dump($res2);
             if (true !== $res2['msg'])  return false;
 
             $Umoney_data = array_merge($Umoney_data,$res2['data']);
