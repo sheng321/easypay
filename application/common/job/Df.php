86f775ff9ca57952ec10fb666583dac1ad2fa5df
jiashen321@protonmail.com <tanjiashen321>
2020-03-05 15:29:12 +0800

    tanger

--- application/common/job/Df.php
+++ application/common/job/Df.php
@@ -158,6 +158,7 @@ class Df {
         //确认数据是否更新完成
         $status = \app\common\model\Df::where(['id'=>$Order['id']])->value('status');
         dump($status);
+        dump(7);
         if($status > 2) return true;
         return false;
     }
