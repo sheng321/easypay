eb9634d49f327c8e7e5910a8da7ebb40ce40cfa9
jiashen321@protonmail.com <tanjiashen321>
2020-03-06 17:46:47 +0800

    tanger

--- application/common/job/Df.php
+++ application/common/job/Df.php
@@ -54,7 +54,7 @@ class Df {
                     return $isJobDone;
                  },$lock_val);
         }catch (\Exception $e){
-            halt($e);
+            halt($e->getMessage());
             $job->delete();//出现异常
             return;
         }
