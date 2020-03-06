f2cc5c97d7c1dc87aa507ad7044fe047ff73d896
jiashen321@protonmail.com <tanjiashen321>
2020-03-06 15:58:23 +0800

    tanger

--- application/admin/controller/Test.php
+++ application/admin/controller/Test.php
@@ -20,33 +20,8 @@ class Test  extends Controller
     public function index()
     {
 
-
-        $RedisModel = StringModel::instance();
-        $RedisModel->select(0);
-
-        try{
-            $str = '11';
-        $sta =    Lock::queueLock(function ($res){
-                return 777;
-            },$str);
-        }catch (\Exception $e){
-            halt($e->getMessage());
-        }
-
-        halt($sta);
-
-
-
-
-
-
-
-        //$res = SysAdmin::delRedis(1);
-
-        //halt( $res);
-
-       // $res = \think\Queue::push('app\\common\\job\\Df', 289, 'df');
-       // halt( $res);
+        $res = \think\Queue::push('app\\common\\job\\Df', 334, 'df');
+        halt( $res);
 
 
     }
