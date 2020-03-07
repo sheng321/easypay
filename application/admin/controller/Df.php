cce7b22d2008eef82512f558e6c62ce8e5bcf899
jiashen321@protonmail.com <tanjiashen321>
2020-03-07 15:23:56 +0800

    tanger

--- application/admin/controller/Df.php
+++ application/admin/controller/Df.php
@@ -250,7 +250,7 @@ class Df extends AdminController {
                         Db::startTrans();
                         $save1 = $this->model->save($post, ['id' => $post['id']]);
                         $save = (new Umoney())->isUpdate(true)->saveAll($Umoney_data);
-                        $add = (new UmoneyLog())->isUpdate(false)->insertAll($UmoneyLog_data);
+                        $add = (new UmoneyLog())->insertAll($UmoneyLog_data);
                         if (!$save1 || !$save || !$add) throw new \Exception('数据有误，请稍后再试!');
                         Db::commit();
                         //添加异步查询订单状态
