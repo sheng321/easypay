e6298392ed5ab884db44042dc6f2a15f6643aff5
jiashen321@protonmail.com <tanjiashen321>
2020-03-08 09:48:14 +0800

    tanger

--- application/admin/controller/api/Crontab.php
+++ application/admin/controller/api/Crontab.php
@@ -47,13 +47,13 @@ class Crontab  extends Controller
             ->where('id', 'IN', function ($query) {
                 $query->table('cm_withdrawal')->where([['status', '>',1]])->field('id')->order(['id'=>'desc']);
             })->count(1);
-        if(!empty($num))  addTask('结算提现通知',"有 $num 提现订单未处理。",6,3);
+        if(!empty($num))  addTask('结算提现通知',"有{$num}笔提现订单未处理。",6,3);
 
         $num1 = Db::table('cm_withdrawal_api')
             ->where('id', 'IN', function ($query) {
                 $query->table('cm_withdrawal_api')->where([['status', '>',1]])->field('id')->order(['id'=>'desc']);
             })->count(1);
-        if(!empty($num1))  addTask('代付提现通知',"有 $num1 代付提现订单未处理。",6,3);
+        if(!empty($num1))  addTask('代付提现通知',"有{$num1}笔代付提现订单未处理。",6,3);
 
         echo '更新成功';
     }
