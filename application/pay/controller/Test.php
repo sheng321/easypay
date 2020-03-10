8218b8107e7f69e2ffcd712ced0f5f5e8958833c
jiashen321@protonmail.com <tanjiashen321>
2020-03-10 09:00:09 +0800

    tanger

--- application/pay/controller/Test.php
+++ application/pay/controller/Test.php
@@ -9,6 +9,8 @@ class Test extends Controller
     public function index(){
       $token = $this->request->get('token/s','原样返回字段');
       $has = Cache::get('pay_token','');
+      dump($token);
+      halt($has);
       if(md5($has) != md5($has)) $this->redirect('http://www.baidu.com');
 
         $pay_memberid = config('set.memberid');   //商户ID
