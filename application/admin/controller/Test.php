4c234ce60cfaef42fadf41a11c462817f114dbfa
jiashen321@protonmail.com <tanjiashen321>
2020-03-10 18:52:45 +0800

    tanger

--- application/admin/controller/Test.php
+++ application/admin/controller/Test.php
@@ -21,13 +21,7 @@ class Test  extends Controller
     public function index()
     {
 
-        $res =  MoneyService::api('s2003091536151522649');
-
-       // exceptions_api('当前访问人数过多，请稍后再试~');
-
-
-       //  $res = \think\Queue::push('app\\common\\job\\Df', 363, 'df');
-        halt( $res);
+        halt(get_client_ip());
 
 
     }
