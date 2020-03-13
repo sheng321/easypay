12a4ec604026065fb36b726d8c15b18d5e9ddb3d
jiashen321@protonmail.com <tanjiashen321>
2020-03-13 12:44:24 +0800

    tanger

--- application/pay/controller/Api.php
+++ application/pay/controller/Api.php
@@ -30,13 +30,13 @@ class Api extends PayController
             $num1 = count($orderId);
             if($num1 > 9){
                 if($num1 < 16){
-                    $orderIdType = is_numeric(implode('',$orderId)); //是否数字
+                    $orderIdType = ctype_alnum(implode('',$orderId)); //检查字符串是否是数字和字母，或者两者混合
                     if($orderIdType){
                         $num =  Order::where([['out_trade_no','in',$orderId],['pay_status','=',2]])->count(1);//是否有支付的情况
                         if(empty($num)) __jerror('系统检测到存在刷单的情况，请稍后在试1！！');
                         $orderId = [];//有支付的情况
                     }else{
-                        //不是数字，表明受到了攻击
+                        //不是，表明受到了攻击
                         __jerror('系统检测到存在刷单的情况，请稍后在试2！！');
                     }
                 }else{
