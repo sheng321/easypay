7fd07b082d97a2fef250c26974929a0e5eb1dc9c
jiashen321@protonmail.com <tanjiashen321>
2020-03-18 16:22:34 +0800

    tanger

--- application/admin/controller/Test.php
+++ application/admin/controller/Test.php
@@ -22,7 +22,15 @@ class Test  extends Controller
     {
 
 
-        halt('您当前的IP是 '.get_client_ip());
+        dump('您当前的IP是 '.get_client_ip());
+        $ip = get_client_ip();
+
+        $ip_start = bindec(decbin(ip2long('175.176.40.1'))); //起始ip
+        $ip_end = bindec(decbin(ip2long('175.176.41.255')));//结束ip
+        //可以这样简单判断
+        if($ip < $ip_start || $ip >$ip_end){
+            dump(222222);
+        }
 
 
     }
